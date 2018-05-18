6b534194238d29643b683320866f967352a86293
MandarGade <mandargade.2997@gmail.com>
2018-05-17 22:35:27 -0700

    'changing stucture'

---
+++ my_square_meal_server.py
@@ -0,0 +1,51 @@
+from flask import Flask,request
+from server.recommendation_pickle import recommendationPickle
+from server.data_operations import operations
+from server.reverse_geocoding import city_name
+from flask_cors import CORS
+import json
+
+
+app = Flask(__name__)
+CORS(app)
+MENU_API_LIST=[]
+
+@app.route("/")
+def index():
+    return "mySquareMealServer ... !!!"
+
+@app.route("/recommendation",methods=['POST','GET'])
+def getRecommendation():
+    #lat = request.args.get("latitude")
+    #long = request.args.get("longitude")
+    request_data = request.get_json()
+    email=request_data.get('email')
+    lat=request_data.get('latitude')
+    long=request_data.get('longitude')
+    city = city_name.get_city_name(lat,long)
+    print(lat,long,city)
+    check_city = operations.check_city_list(city=city)
+    print("City Present in DB: "+str(check_city))
+    print("email: "+email)
+    if check_city:
+        nearby_restaurants = operations.get_nearby(lat=lat,long=long,city=city)
+        menu_api, menu_list = operations.get_menu_items(nearby_restaurants)
+        list_of_apis, list_of_menus = operations.filter_for_user_allergies(email,menu_api, menu_list)
+        print(len(list_of_apis))
+        result_api_list = recommendationPickle.get_recommendation_from_pickle(list_of_apis, list_of_menus)
+        result = operations.get_result_data(result_api_list,city)
+        return json.dumps(result)
+    else:
+        return "city not found in database"
+
+
+@app.route("/nutrition-recommendation",methods=['GET'])
+def getNutritionInfo():
+    print(MENU_API_LIST)
+    return "success"
+
+
+if __name__=='__main__':
+    app.run(debug=True, port=8001)
+    #data_operations_object = operations()
+    #pickle_object = recommendationPickle()
--- server/recommendation_pickle.py
+++ server/recommendation_pickle.py
@@ -6,21 +6,22 @@ class recommendationPickle:
 
     def get_recommendation_from_pickle(menu_api, list_of_ingredients):
         testData = []
+
         for each in list_of_ingredients:
             ingredients_string = each.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
             ingredients_string = ingredients_string.replace('"', ' ')
             ingredients_string = ingredients_string.translate({ord(c): " " for c in '0123456789'})
             testData.append(ingredients_string)
 
+
         testDataFrame = pd.DataFrame(testData, columns=['ingredients'])
         testDataFrame = testDataFrame.replace('-', ' ', regex=True)
         x_test = testDataFrame.iloc[:, 0].values
-        vect = joblib.load('../pickles/vector_pickle.pkl')
+        vect = joblib.load('pickles/vector_pickle.pkl')
         x_test_vectorized = vect.transform(x_test)
 
-        clf = joblib.load('../pickles/prediction_pickle.pkl')
+        clf = joblib.load('pickles/prediction_pickle.pkl')
         result = clf.predict(x_test_vectorized)
-
         index_list = [index for index, value in enumerate(result) if value == 1]
         result_api = []
         for each in index_list:
